433b0c8bd503c2402ff3d30e4d0d364a8aa4d8bb
Gerardo Garza <owlscloud@gmail.com>
2023-05-30 21:07:34 -0500

    a

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
